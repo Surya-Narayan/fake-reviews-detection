33f530cd208ea37b69fce23ec5022cebd2ae9118
Surya Narayan <surya.narayan0707@gmail.com>
2023-04-09 23:27:46 -0700

    Update Random_Forrest.py

--- CODE/Random_Forrest.py
+++ CODE/Random_Forrest.py
@@ -1,36 +1,33 @@
+import os.path
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
+import pandas as pd
+import numpy as np
 from sklearn import metrics
-import matplotlib.pyplot as plt
+from scipy.stats import entropy
 import seaborn as sns
-from sklearn.decomposition import PCA
-import pandas as pd
-import numpy as np
-import nltk
-#from feature_extraction import *
-import pandas as pd
-import numpy as np
-import string
 import re
 import collections
-from statistics import mean
+import string
+from sklearn.metrics import roc_curve, auc
+from imblearn.under_sampling import NearMiss
 from nltk.tokenize import sent_tokenize
+from statistics import mean
+import matplotlib as mpl
+from feature_extraction import *
+from sklearn.decomposition import PCA
+import nltk
+from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import pairwise_distances
-from scipy.stats import entropy
-from imblearn.under_sampling import NearMiss
-from sklearn.preprocessing import Normalizer
 import joblib
-from sklearn.metrics import roc_curve, auc
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib.legend_handler import HandlerLine2D
 import plotly.express as px
-import os.path
-from feature_extraction import *
+from matplotlib.legend_handler import HandlerLine2D
+from sklearn.preprocessing import Normalizer
 
 
 # perform undersampling
@@ -49,18 +46,27 @@ from feature_extraction import *
 
 #     return feature_importance(clf,feature_names)
 
-def train_rf(train_data, validation_data, train_target, validation_target, feature_list, type="rf"):
-    root_dir = os.getcwd()
-    model_path = os.path.join(root_dir, 'CODE', 'models', 'rf_v1.joblib')
-    if not os.path.exists(model_path):
-        random_forest = RandomForestClassifier(n_estimators=64, max_depth=10)
-        random_forest.fit(train_data, train_target)
-        joblib.dump(random_forest, model_path)
-    random_forest = joblib.load(model_path)
-    predicted_values = random_forest.predict(validation_data)
-    evaluate(validation_target, predicted_values, type)
+def train_rf(training_features, testing_features, training_labels, testing_labels, feature_name_list, rf_model_type="rf"):
+    project_directory = os.getcwd()
+    rf_model_file_path = os.path.join(project_directory, 'CODE', 'models', 'rf_v1.joblib')
+    
+    def create_rf_classifier():
+        rf_classifier = RandomForestClassifier(n_estimators=64, max_depth=10)
+        rf_classifier.fit(training_features, training_labels)
+        return rf_classifier
+    
+    if not os.path.exists(rf_model_file_path):
+        rf_classifier_model = create_rf_classifier()
+        joblib.dump(rf_classifier_model, rf_model_file_path)
+    else:
+        with open(rf_model_file_path, 'rb') as classifier_file:
+            rf_classifier_model = joblib.load(classifier_file)
+        
+    predictions_from_classifier = rf_classifier_model.predict(testing_features)
+    evaluate(testing_labels, predictions_from_classifier, rf_model_type)
 
-    return feature_importance(random_forest, feature_list)
+    return feature_importance(rf_classifier_model, feature_name_list)
+
 
 
 # def feature_importance(clf,feature_names):
@@ -86,27 +92,26 @@ def train_rf(train_data, validation_data, train_target, validation_target, featu
     
 #     return [importances['Features'][i] for i in range(10)]
 
-def feature_importance(model, feature_list):
-    feature_significance_dict = {}
-    for feat, significance in zip(feature_list, model.feature_importances_):
-        feature_significance_dict[feat] = significance
-    significance_df = pd.DataFrame.from_dict(feature_significance_dict, orient='index').rename(columns={0: 'Gini-Significance'})
-    significance_df = significance_df.sort_values(by='Gini-Significance', ascending=False)
-    significance_df = significance_df.reset_index()
-    significance_df = significance_df.rename(columns={'index': 'Feature_Name'})
+def feature_importance(trained_model, feature_names):
+    feature_importance_pairs = list(zip(feature_names, trained_model.feature_importances_))
+    sorted_feature_importance = sorted(feature_importance_pairs, key=lambda x: x[1], reverse=True)
+    
+    importance_df = pd.DataFrame(sorted_feature_importance, columns=['Feature_Name', 'Gini-Importance'])
+    
     sns.set(font_scale=5)
     sns.set(style="darkgrid", color_codes=True, font_scale=1.7)
     fig, ax = plt.subplots()
     fig.set_size_inches(30, 15)
-    sns.barplot(x=significance_df['Gini-Significance'], y=significance_df['Feature_Name'], data=significance_df, color='magenta')
-    plt.xlabel('Significance', fontsize=25, weight='bold')
+    sns.barplot(x=importance_df['Gini-Importance'], y=importance_df['Feature_Name'], data=importance_df, color='blue')
+    plt.xlabel('Importance', fontsize=25, weight='bold')
     plt.ylabel('Feature_Name', fontsize=25, weight='bold')
-    plt.title('Feature Significance', fontsize=25, weight='bold')
-    plt.savefig(f'{os.getcwd()}/EVALUATIONS/RF_feature_significance.png')
+    plt.title('Feature Importance', fontsize=25, weight='bold')
+    plt.savefig(f'{os.getcwd()}/EVALUATIONS/RF_feature_importance.png')
     plt.show()
 
-    result = [significance_df['Feature_Name'][i] for i in range(10)]
-    return result
+    top_features = [importance_df['Feature_Name'][i] for i in range(10)]
+    return top_features
+
     
 # def pca_visualization(train_features,labels):
 #     pca = PCA(n_components=2)
@@ -114,36 +119,67 @@ def feature_importance(model, feature_list):
 #     fig = px.scatter(components, x=0, y=1, color=labels)
 #     fig.show()
 
-def pca_visualization(train_data, target_labels):
-    reduced_pca = PCA(n_components=3)
-    principal_components = reduced_pca.fit_transform(train_data)
-    scatter_plot = px.scatter_3d(principal_components, x=0, y=1, z=2, color=target_labels)
-    scatter_plot.write_image("./EVALUATIONS/RF_PCA_Visualization.png")
-    scatter_plot.show()
+def pca_visualization(input_data, target_labels):
+    pca_reducer = PCA(n_components=3)
+    transformed_data = pca_reducer.fit_transform(input_data)
+    df_transformed_data = pd.DataFrame(transformed_data, columns=['PC1', 'PC2', 'PC3'])
+    df_transformed_data['labels'] = target_labels
+
+    fig = px.scatter_3d(df_transformed_data, x='PC1', y='PC2', z='PC3', color='labels')
+    fig.write_image("./EVALUATIONS/RF_PCA_3D_Visualization.png")
+    fig.show()
+
+
+# def compare_rf(x_train_data, y_train_data, x_test_data, y_test_data):
+#     num_estimators = [1, 2, 4, 8, 16, 32, 64, 100, 200]
+#     max_tree_depths = [1, 2, 3, 4, 8, 9, 10, 16, 32]
+#     train_scores = []
+#     test_scores = []
+    
+#     for idx, estimator in enumerate(num_estimators):
+#         rf_classifier = RandomForestClassifier(n_estimators=estimator, max_depth=max_tree_depths[idx])
+#         rf_classifier.fit(x_train_data, y_train_data)
+#         train_predictions = rf_classifier.predict(x_train_data)
+#         fpr_train, tpr_train, _ = roc_curve(y_train_data, train_predictions)
+#         train_auc = auc(fpr_train, tpr_train)
+#         train_scores.append(train_auc)
+        
+#         test_predictions = rf_classifier.predict(x_test_data)
+#         fpr_test, tpr_test, _ = roc_curve(y_test_data, test_predictions)
+#         test_auc = auc(fpr_test, tpr_test)
+#         test_scores.append(test_auc)
+        
+#     train_line, = plt.plot(num_estimators, train_scores, 'g', label="Train AUC")
+#     test_line, = plt.plot(num_estimators, test_scores, 'r', label="Test AUC")
+#     plt.legend(handler_map={train_line: HandlerLine2D(numpoints=2)})
+#     plt.ylabel('Area Under Curve Score')
+#     plt.xlabel('Number of Estimators')
+#     plt.savefig(f'{os.getcwd()}/EVALUATIONS/RF_accuracy_scores.png')
+#     plt.show()
 
 def compare_rf(x_train_data, y_train_data, x_test_data, y_test_data):
     num_estimators = [1, 2, 4, 8, 16, 32, 64, 100, 200]
     max_tree_depths = [1, 2, 3, 4, 8, 9, 10, 16, 32]
     train_scores = []
     test_scores = []
-    
-    for idx, estimator in enumerate(num_estimators):
-        rf_classifier = RandomForestClassifier(n_estimators=estimator, max_depth=max_tree_depths[idx])
+
+    for estimator, depth in zip(num_estimators, max_tree_depths):
+        rf_classifier = RandomForestClassifier(n_estimators=estimator, max_depth=depth)
         rf_classifier.fit(x_train_data, y_train_data)
-        train_predictions = rf_classifier.predict(x_train_data)
-        fpr_train, tpr_train, _ = roc_curve(y_train_data, train_predictions)
-        train_auc = auc(fpr_train, tpr_train)
+        
+        train_pred_prob = rf_classifier.predict_proba(x_train_data)[:, 1]
+        test_pred_prob = rf_classifier.predict_proba(x_test_data)[:, 1]
+
+        train_auc = roc_auc_score(y_train_data, train_pred_prob)
+        test_auc = roc_auc_score(y_test_data, test_pred_prob)
+
         train_scores.append(train_auc)
-        
-        test_predictions = rf_classifier.predict(x_test_data)
-        fpr_test, tpr_test, _ = roc_curve(y_test_data, test_predictions)
-        test_auc = auc(fpr_test, tpr_test)
         test_scores.append(test_auc)
-        
-    train_line, = plt.plot(num_estimators, train_scores, 'g', label="Train AUC")
-    test_line, = plt.plot(num_estimators, test_scores, 'r', label="Test AUC")
-    plt.legend(handler_map={train_line: HandlerLine2D(numpoints=2)})
+
+    plt.plot(num_estimators, train_scores, 'g', label="Train AUC")
+    plt.plot(num_estimators, test_scores, 'r', label="Test AUC")
+    plt.legend(loc='lower right')
     plt.ylabel('Area Under Curve Score')
     plt.xlabel('Number of Estimators')
-    plt.savefig(f'{os.getcwd()}/EVALUATIONS/RF_accuracy_scores.png')
+    plt.savefig(f'{os.getcwd()}/EVALUATIONS/RF_auc_scores.png')
     plt.show()
