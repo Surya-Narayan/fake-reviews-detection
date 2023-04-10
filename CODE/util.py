6dd6101d4feaac1fb23aa77fb9afedd1f70dc63c
Surya Narayan <surya.narayan0707@gmail.com>
2023-04-09 23:11:41 -0700

    util.py and removed feature_importance.png

--- CODE/util.py
+++ CODE/util.py
@@ -2,15 +2,14 @@
 import csv
 from math import sqrt, pow, exp
 
+def text_to_csv(input_file_name):
+    with open(f"./YelpZip/{input_file_name}.txt", encoding='cp850') as source_file, \
+            open(f"{input_file_name}.csv", mode='w', newline='', encoding='cp850') as target_file:
+        
+        csv_writer = csv.writer(target_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        
+        for row in source_file:
+            csv_writer.writerow(row.strip().split('\t'))
 
-#converts tab separated .txt file to .csv
-def text_to_csv(filename):
-
-    with open(f"./YelpZip/{filename}.txt", encoding='cp850') as f:
-        text = f.read().split('\n')
-
-    with open('{filename}.csv', mode='w', newline='', encoding='cp850') as content_file:
-        content_writer = csv.writer(content_file, delimiter=',', quotechar='"',  quoting=csv.QUOTE_MINIMAL)
-
-        for line in text:
-            content_writer.writerow(line.split('\t'))
+# Example usage
+# transform_txt_to_csv('example_filename')
