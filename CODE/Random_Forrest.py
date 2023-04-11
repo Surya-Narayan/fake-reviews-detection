82be2b4e5b954d77257e981f24668b4926d468f8
Surya Narayan <surya.narayan0707@gmail.com>
2023-04-10 17:15:20 -0700

    updated parameters for random forest

--- CODE/Random_Forrest.py
+++ CODE/Random_Forrest.py
@@ -21,13 +21,58 @@ import joblib
 import plotly.express as px
 
 
+def compare_rf_plot(num_estimators, train_scores, test_scores):
+    """
+    This function creates a plot to visualize the performance of the RandomForestClassifier model with different hyperparameter configurations.
+    It takes as input the number of estimators, train scores, and test scores, and generates a plot showing the Area Under the Curve (AUC) scores for both the train and test datasets.
+    The plot is saved as an image in the 'EVALUATIONS' directory.
+    """
+    plt.plot(num_estimators, train_scores, 'g', label="Train AUC")
+    plt.plot(num_estimators, test_scores, 'r', label="Test AUC")
+    plt.legend(loc='lower right')
+    plt.ylabel('Area Under Curve Score')
+    plt.xlabel('Number of Estimators')
+    plt.savefig(f'{os.getcwd()}/EVALUATIONS/RF_auc_scores.png')
+    plt.show()
+
+def feature_importance_plot(importance_df):
+    """
+    This function creates a bar plot to visualize the feature importance of a model, given a DataFrame with feature names and their corresponding Gini-importance values.
+    The plot displays the importance of each feature on the y-axis and the feature names on the x-axis.
+    The plot is saved as an image in the 'EVALUATIONS' directory.
+    """
+    sns.set(style="darkgrid", color_codes=True, font_scale=2)
+    fig, ax = plt.subplots()
+    fig.set_size_inches(50, 25)
+    sns.barplot(x=importance_df['Gini-Importance'], y=importance_df['Feature_Name'], data=importance_df, color='magenta')
+    plt.xlabel('Importance', fontsize=18)
+    plt.ylabel('Feature_Name', fontsize=18)
+    plt.title('Feature Importance', fontsize=22, weight='bold')
+    plt.savefig(f'{os.getcwd()}/EVALUATIONS/RF_feature_importance.png')
+    plt.show()
+
 # perform undersampling
 def train_rf(training_features, testing_features, training_labels, testing_labels, feature_name_list, type="rf"):
+    """
+    This function trains a RandomForestClassifier model with the given training features and labels. If a saved model already exists, it loads the model from disk.
+    The function evaluates the model using the testing features and labels, and returns the feature importances.
+    
+    Parameters:
+    training_features (array-like): The training dataset features.
+    testing_features (array-like): The testing dataset features.
+    training_labels (array-like): The training dataset labels.
+    testing_labels (array-like): The testing dataset labels.
+    feature_name_list (list): A list of feature names for the dataset.
+    type (str, optional): A string representing the model type. Default is "rf" (Random Forest).
+    
+    Returns:
+    DataFrame: A DataFrame containing the feature importances.
+    """    
     project_directory = os.getcwd()
     rf_model_file_path = os.path.join(project_directory, 'CODE', 'models', 'rf_v1.joblib')
     
     def create_rf_classifier():
-        rf_classifier = RandomForestClassifier(n_estimators=64, max_depth=10)
+        rf_classifier = RandomForestClassifier(n_estimators=56, max_depth=10)
         rf_classifier.fit(training_features, training_labels)
         return rf_classifier
     
@@ -44,26 +89,35 @@ def train_rf(training_features, testing_features, training_labels, testing_label
     return feature_importance(rf_classifier_model, feature_name_list)
 
 def feature_importance(trained_model, feature_names):
+    """
+    This function calculates the feature importances for a trained model and generates a bar plot of the feature importances.
+    The function also returns a list of the top 10 features with the highest importance.
+    
+    Parameters:
+    trained_model (fitted model object): A trained model object (e.g., RandomForestClassifier).
+    feature_names (list): A list of feature names for the dataset.
+    
+    Returns:
+    list: A list containing the top 10 features with the highest Gini-importance.
+    """
     feature_importance_pairs = list(zip(feature_names, trained_model.feature_importances_))
     sorted_feature_importance = sorted(feature_importance_pairs, key=lambda x: x[1], reverse=True)
     
     importance_df = pd.DataFrame(sorted_feature_importance, columns=['Feature_Name', 'Gini-Importance'])
-    
-    sns.set(font_scale=5)
-    sns.set(style="darkgrid", color_codes=True, font_scale=1.7)
-    fig, ax = plt.subplots()
-    fig.set_size_inches(30, 15)
-    sns.barplot(x=importance_df['Gini-Importance'], y=importance_df['Feature_Name'], data=importance_df, color='blue')
-    plt.xlabel('Importance', fontsize=25, weight='bold')
-    plt.ylabel('Feature_Name', fontsize=25, weight='bold')
-    plt.title('Feature Importance', fontsize=25, weight='bold')
-    plt.savefig(f'{os.getcwd()}/EVALUATIONS/RF_feature_importance.png')
-    plt.show()
+    feature_importance_plot(importance_df)
 
     top_features = [importance_df['Feature_Name'][i] for i in range(10)]
     return top_features
 
 def pca_visualization(input_data, target_labels):
+    """
+    This function applies Principal Component Analysis (PCA) to reduce the input data to three dimensions and creates a 3D scatter plot of the transformed data.
+    The plot is colored based on the target labels and saved as an image in the 'EVALUATIONS' directory.
+    
+    Parameters:
+    input_data (array-like): The input dataset to be transformed using PCA.
+    target_labels (array-like): The target labels corresponding to the input dataset.
+    """
     pca_reducer = PCA(n_components=3)
     transformed_data = pca_reducer.fit_transform(input_data)
     df_transformed_data = pd.DataFrame(transformed_data, columns=['PC1', 'PC2', 'PC3'])
@@ -74,8 +128,19 @@ def pca_visualization(input_data, target_labels):
     fig.show()
 
 def compare_rf(x_train_data, y_train_data, x_test_data, y_test_data):
-    num_estimators = [1, 2, 4, 8, 16, 32, 64, 100, 200]
-    max_tree_depths = [1, 2, 3, 4, 8, 9, 10, 16, 32]
+    """
+    This function trains a RandomForestClassifier model with different hyperparameter configurations and compares their performance.
+    The hyperparameters used are the number of estimators and maximum tree depth, and the performance is evaluated using Area Under the Curve (AUC) scores.
+    The function calls 'compare_rf_plot' to generate a plot of the AUC scores for the different hyperparameter configurations.
+    
+    Parameters:
+    x_train_data (array-like): The training dataset features.
+    y_train_data (array-like): The training dataset labels.
+    x_test_data (array-like): The testing dataset features.
+    y_test_data (array-like): The testing dataset labels.
+    """
+    num_estimators = [2, 5, 10, 20, 40, 60, 80, 120, 150]
+    max_tree_depths = [1, 3, 4, 6, 8, 11, 13, 18, 25]
     train_scores = []
     test_scores = []
 
@@ -92,10 +157,4 @@ def compare_rf(x_train_data, y_train_data, x_test_data, y_test_data):
         train_scores.append(train_auc)
         test_scores.append(test_auc)
 
-    plt.plot(num_estimators, train_scores, 'g', label="Train AUC")
-    plt.plot(num_estimators, test_scores, 'r', label="Test AUC")
-    plt.legend(loc='lower right')
-    plt.ylabel('Area Under Curve Score')
-    plt.xlabel('Number of Estimators')
-    plt.savefig(f'{os.getcwd()}/EVALUATIONS/RF_auc_scores.png')
-    plt.show()
+    compare_rf_plot(num_estimators, train_scores, test_scores)
